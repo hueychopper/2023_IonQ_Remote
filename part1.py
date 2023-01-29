2cb294519efae537f751ac26e2c65bf0ae0e8ef4
hueychopper <codelearn370@gmail.com>
2023-01-29 14:23:26 +0000

    added

--- part1.py
+++ part1.py
@@ -3,23 +3,19 @@ import qiskit
 import numpy as np
 
 
-def encode_cirq(image):
-    circuit=cirq.Circuit()
-    if image[0][0]==0:
-        circuit.append(cirq.rx(np.pi).on(cirq.LineQubit(0)))
-    return circuit
-
-def encode_qiskit(image):
-    q = qiskit.QuantumRegister(3)
-    circuit = qiskit.QuantumCircuit(q)
-    if image[0][0]==0:
-        circuit.rx(np.pi,0)
-    return circuit
+def encode_qiskit(image,cIndex):
+    qc = qiskit.QuantumCircuit(3)
+    for i in range(len(image)):
+        t = np.unique(image[i], return_index=True)
+        # print(t)
+        if labels[cIndex] == False:
+            qc.x(0)
+    return qc
 
 
-def decode(histogram):
-    if 1 in histogram.keys():
-        image=[[0,0],[0,0]]
-    else:
-        image=[[1,1],[1,1]]
-    return image
+def decode(hist):
+    for key in hist.keys():
+        if hist[key][0] == 1.0:
+            print(True)
+        else:
+            print(False)
